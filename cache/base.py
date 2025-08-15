1d04695724c63ff7685bb71996240f537e7192c3
MukPun <894535259@qq.com>
2025-08-15 18:46:24 +0800

    # 添加LRU代码主体

---
+++ cache/base.py
@@ -0,0 +1,130 @@
+"""
+缓存相关的基础对象
+
+"""
+
+class CCacheItem:
+	"""缓存项
+	类似链表结构中的节点
+	"""
+
+	def __init__(self, obj):
+		self.obj = obj			# 缓存的元素
+		self.prev = None		# 前一个缓存项
+		self.next = None		# 后一个缓存项
+
+
+class CCacheListIter:
+
+	def __init__(self, cache_list):
+		self.head = cache_list.head
+		self.current = self.head.next
+	
+	def next(self):
+		if self.current == self.head:
+			# 意味着缓存列表已经遍历完毕
+			raise StopIteration()
+		item = self.current
+		self.current = self.current.next
+		return item
+
+class CCacheList:
+	"""缓存链表
+	代表同一类别的缓存元素集合
+
+	作用:
+		1.管理缓存项
+		2.封装缓存项的操作接口
+	
+	"""
+
+	def __init__(self, i_max_count=10, func_lru_handle=None):
+		self.head = CCacheItem(None)					# 头结点
+		self.head.prev = self.head.next = self.head	# 构建循环链表	(这样真的不会循环引用吗？)			
+		self.tail = self.head						# 尾节点
+		self.item_count = 0							# 缓存项数量
+
+		self.max_count = i_max_count				# 最大缓存项数量
+		self.lru_handle = func_lru_handle			# 触发LRU时的处理函数
+
+	def __iter__(self):
+		"""
+		# 返回迭代器对象, 使得变成迭代器, 成为可迭代对象
+		# 使缓存项可以遍历
+		"""
+		return CCacheListIter(self)
+
+	def Clear(self):
+		self.head.prev = self.head.next = self.head
+		self.tail = self.head
+		self.item_count = 0
+
+	def AddObject(self, obj):
+		if self.item_count >= self.max_count:
+			self._DoLRU()
+		new_item = CCacheItem(obj)
+		self._AddTotail(new_item)
+		self.item_count += 1
+		return new_item
+
+	def GetObjectFromItem(self, o_item : CCacheItem, last_get_time):
+		if o_item.prev is None or o_item.next is None:
+			return None
+
+		if not o_item is self.tail:
+			# 只要不是尾部的节点, 都刷新到尾部
+			self._RefreshItem(o_item)
+		o_item.obj.last_get = last_get_time
+		return o_item.obj
+
+
+	def DelItem(self, o_item : CCacheItem):
+		if o_item.prev is None or o_item.next is None:
+			return
+		assert o_item != self.head	# 禁止删除头节点
+		self._Unlink(o_item)
+		self.item_count -= 1
+
+	# ========================== 私有方法 Start ==========================
+
+	def _Unlink(self, o_item : CCacheItem):
+		prev = o_item.prev
+		next = o_item.next
+
+		prev.next = next
+		next.prev = prev
+		o_item.prev = o_item.next = None
+		if o_item == self.tail:
+			self.tail = prev
+
+	def _AddToTail(self, o_item : CCacheItem):
+		o_item.prev = self.tail
+		o_item.next = self.head
+
+		self.head.prev = o_item
+		self.tail.next = o_item
+		self.tail = o_item
+
+	def _DoLRU(self):
+		assert self.item_count >= 1	
+		head_item = self.head.next
+		self.DelItem(head_item)
+		if self.lru_handle is not None:
+			self.lru_handle(head_item.obj)
+
+	def _RefreshItem(self, o_item : CCacheItem):
+		self._Unlink(o_item)
+		self._AddToTail(o_item)
+
+	# =========================== 私有方法 End ==========================
+
+
+class CCacheObject:
+	"""缓存对象
+	用于缓存数据, 缓存的最小单元,包含实际的缓存数据
+	
+	"""
+	def __init__(self, k, v, now):
+		self.key = k
+		self.value = v
+		self.last_get = now				# 最后一次被使用的时间
--- cache/lru.py
+++ cache/lru.py
@@ -8,5 +8,109 @@
 
 """
 
+import time
+
+from .base import CCacheObject, CCacheList
+
 class CLRUCache:
-    pass
+	
+	def __init__(self, capacity, lru_handle=None, update_interval=0):
+		"""
+		初始化LRU缓存管理器
+		:param capacity: 需要进行缓存的容器对象最大数量
+		:param lru_handle: 缓存对象处理函数
+		:param update_interval: 缓存对象更新间隔		# 启动定时器,定时刷新 self._now(本意是用于每次访问缓存、增加缓存时使用的时间戳,避免每次操作时都进行时间获取,提高效率。由于与定时器功能耦合,增加即时获取时间戳的方案。)
+		"""
+		
+		self._dict = {}		 		# 用于快速检索缓存项的hash表(要在使用层注意key的唯一性,否则会进行覆盖,且被覆盖的缓存需要等到下一次LRU时才会被清除)
+		self._cache_list = CCacheList(capacity, func_lru_handle=self.OnDiscard)
+		self._lru_handle = lru_handle
+		self._now = int(time.time())
+
+		if update_interval > 0:
+			pass
+		else:
+			self._update_timer = None
+
+	# ====================== 魔术方法 Start ======================
+
+	def __len__(self):
+		"""允许通过len(obj)获取缓存数量
+		"""
+		return len(self._dict)
+
+	def __contains__(self, key):
+		"""允许通过key in obj判断缓存中是否存在key
+		"""
+		return key in self._dict
+	
+	# ====================== 魔术方法 End ======================
+
+
+	def Add(self, key, value):
+		""" 添加缓存
+		要在使用层注意key的唯一性,否则会进行覆盖,且被覆盖的缓存需要等到下一次LRU时才会被清除
+		"""
+		cache_item = self._cache_list.AddObject(CCacheObject(key, value, self._GetTime()))
+		self._dict[key] = cache_item
+
+	def Pop(self, key):
+		""" 弹出缓存
+		"""
+		cache_item = self._dict.pop(key, None)
+		if cache_item is None:
+			return None
+		self._cache_list.DelItem(cache_item)
+		return cache_item.obj.value
+
+	def Get(self, key):
+		""" 访问缓存
+		"""
+		cache_item = self._dict.get(key)
+		if cache_item is None:
+			return None
+		cache_obj = self._cache_list.GetObjectFromItem(cache_item, self._GetTime())
+		return cache_obj.value 
+
+	def OnDiscard(self, pair: CCacheObject):
+		""" 缓存对象被丢弃时调用
+		"""
+		self._dict.pop(pair.key, None)
+		if self._lru_handle is not None:
+			self._lru_handle(pair.value)
+
+
+	def raw_cache_iteritems(self):
+		""" 按head到tail的顺序 迭代缓存对象的所有属性
+		"""
+		for cache_item in self._cache_list:
+			cache_obj = cache_item.obj
+			yield cache_obj.key, cache_obj.value, cache_obj.last_get
+
+	def itervalues(self):
+		""" 迭代所有缓存对象的值
+		"""
+		for cache_item in self._dict.itervalues():
+			yield cache_item.obj.value
+
+	def iterkeys(self):
+		""" 迭代所有缓存的键
+		"""
+		for key in self._dict.iterkeys():
+			yield key
+
+	def iteritems(self):
+		""" 迭代所有缓存对象的键值对
+		"""
+		for key, cache_item in self._dict.iteritems():
+			yield key, cache_item.obj.value
+
+
+
+
+	# ====================== 私有方法 Start ======================
+	def _GetTime(self):
+		if self._update_timer:
+			return self._now
+		return int(time.time())
+	# ====================== 私有方法 End ======================
